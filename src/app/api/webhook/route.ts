8c6faa2ab23fa4b8639b82e6c6ca0d77d0748685
Duchieu7802 <duchieu7802@gmail.com>
2025-05-09 09:21:21 +0700

    fix webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -7,10 +7,16 @@ import { Webhook } from "svix";
 const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
 export async function POST(req: Request) {
-	const svix_id = (await headers()).get("svix-id") ?? "";
-	const svix_timestamp = (await headers()).get("svix-timestamp") ?? "";
-	const svix_signature = (await headers()).get("svix-signature") ?? "";
-
+	const headersList = await headers();
+	const svix_id = headersList.get("svix-id") ?? "";
+	const svix_timestamp = headersList.get("svix-timestamp") ?? "";
+	const svix_signature = headersList.get("svix-signature") ?? "";
+	if (!process.env.WEBHOOK_SECRET) {
+		throw new Error("WEBHOOK_SECRET is not set");
+	}
+	if (!svix_id || !svix_timestamp || !svix_signature) {
+		return new Response("Bad Request", { status: 400 });
+	}
 	const payload = await req.json();
 	const body = JSON.stringify(payload);
 
@@ -24,6 +30,7 @@ export async function POST(req: Request) {
 			"svix-timestamp": svix_timestamp,
 			"svix-signature": svix_signature,
 		}) as WebhookEvent;
+		console.log(msg.data);
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (error) {
 		return new Response("Bad Request", { status: 400 });
@@ -31,18 +38,31 @@ export async function POST(req: Request) {
 
 	const eventType = msg.type;
 	if (eventType === "user.created") {
-		const { email_addresses, username, image_url, id } = msg.data;
-		const user = createUser({
-			clerkId: id,
-			email: email_addresses[0].email_address,
-			username: username!,
-			name: username!,
-			avatar: image_url,
-		});
-		return NextResponse.json({
-			message: "Ok",
-			user,
-		});
+		try {
+			const { email_addresses, username, image_url, id } = msg.data;
+			console.log("Data gửi vào createUser:", {
+				clerkId: id,
+				email: email_addresses[0].email_address,
+				username: username!,
+				name: username!,
+				avatar: image_url,
+			});
+			const user = await createUser({
+				clerkId: id,
+				email: email_addresses[0].email_address,
+				username: username!,
+				name: username!,
+				avatar: image_url,
+			});
+			return NextResponse.json({
+				message: "Ok",
+				user: user,
+				msg,
+			});
+		} catch (error) {
+			console.error("Lỗi khi tạo user:", error);
+			return NextResponse.json({ error: "Tạo user thất bại" }, { status: 500 });
+		}
 	}
 	// Rest
 
